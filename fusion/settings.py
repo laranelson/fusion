5313c8a547ee69150f3f8acd8c3c3c820dbd9cb7
Nelson <nelsonlarajr@gmail.com>
2022-07-12 21:35:45 -0300

    adminlte3

--- fusion/settings.py
+++ fusion/settings.py
@@ -35,8 +35,10 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'core',
 
-    #'django_adminlte',
-    #'django_admimlte_theme',
+    # General use templates & template tags (should appear first)
+    'adminlte3',
+    # Optional: Django admin theme (must be before django.contrib.admin)
+    'adminlte3_theme',
     
     'django.contrib.admin',
     'django.contrib.auth',
